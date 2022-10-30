b9328f83c86efd30c6efe26249cf5891c4729381
ankith915 <ankithbhat63@gmail.com>
2022-10-30 15:28:52 +0530

    changes in myworld folder

--- myworld/members/admin.py
+++ myworld/members/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Students
+from .models import Employees
 
-class DjStudentAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "address", "roll_number", "mobile", "branch")
-    list_filter = ("branch",)
+class DjEmployeeAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "address", "employee_id", "mobile", "salary","department")
+    list_filter = ("department",)
 
 # Register your models here.
-admin.site.register(Students, DjStudentAdmin)
+admin.site.register(Employees, DjEmployeeAdmin) 
--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,44 +1,46 @@
 from django.views import View
-from .models import Students
+from .models import Employees
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class StudentView(View):
+class EmployeeView(View):
 
-    def get(self, request, rolno=None, branch=None):
-        student_model_list = []
+    def get(self, request, emp_id=None, department=None):
+        employee_model_list = []
         try:
-            if rolno:
-                student_model_list = Students.objects.filter(roll_number=rolno)
-            elif branch:
-                student_model_list = Students.objects.filter(branch=branch)
-        except Students.DoesNotExist:
-            return JsonResponse({'status': 'failed', "students": None}, status=400)
-        students = []
-        for student in student_model_list:
+            if emp_id:
+                employee_model_list = Employees.objects.filter(employee_id=emp_id)
+            elif department:
+                employee_model_list = Employees.objects.filter(department=department)
+        except Employees.DoesNotExist:
+            return JsonResponse({'status': 'failed', "employees": None}, status=400)
+        employees = []
+        for employee in employee_model_list:
             data = {
-                "first_name" : student.first_name,
-                "last_name": student.last_name,
-                "address": student.address,
-                "roll_number": student.roll_number,
-                "mobile": student.mobile,
-                "branch": student.branch
+                "first_name" : employee.first_name,
+                "last_name": employee.last_name,
+                "address": employee.address,
+                "employee_id": employee.employee_id,
+                "mobile": employee.mobile,
+                "salary": employee.salary,
+                "department": employee.department
             }
-            students.append(data)
-        return JsonResponse({'status': 'success', "students": students}, status=200)
+            employees.append(data)
+        return JsonResponse({'status': 'success', "employees": employees}, status=200)
 
     def post(self, request):
-        if not request.POST.get('first_name') or not request.POST.get('last_name') or not request.POST.get('address') or  not request.POST.get('roll_number') or not request.POST.get('mobile'):
+        if not request.POST.PUT.DELETE.get('first_name') or not request.POST.PUT.DELETE.get('last_name') or not request.POST.PUT.DELETE.get('address') or  not request.POST.PUT.DELETE.get('employee_id') or not request.POST.PUT.DELETE.get('mobile') or not request.POST.PUT.DELETE.get('salary'):
             return JsonResponse({'status': 'failed', "message" : "all fields required"}, status=500)
 
-        Students.objects.create(
-            first_name= request.POST.get('first_name'),
-            last_name= request.POST.get('last_name'),
-            address= request.POST.get('address'),
-            roll_number= request.POST.get('roll_number'),
-            mobile= request.POST.get('mobile'),
-            branch= request.POST.get('branch'))
+        Employees.objects.create(
+            first_name= request.POST.PUT.DELETE.get('first_name'),
+            last_name= request.POST.PUT.DELETE.get('last_name'),
+            address= request.POST.PUT.DELETE.get('address'),
+            employee_id= request.POST.PUT.DELETE.get('employee_id'),
+            mobile= request.POST.PUT.DELETE.get('mobile'),
+            salary= request.POST.PUT.DELETE.get('salary'),
+            department= request.POST.PUT.DELETE.get('department'))
         return JsonResponse({'status': 'sucess'}, status=200)
