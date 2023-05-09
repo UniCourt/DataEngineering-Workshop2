266b32979f632d39682d4c50f6997a1205d2dd1c
Prajwal <prajwal@Ubuntu1.myguest.virtualbox.org>
2023-05-09 17:41:43 +0530

    primary_key_mod

--- Vivekananda_session2_4VP20CS062_Prajwal/EmpManagement/employees/models.py
+++ Vivekananda_session2_4VP20CS062_Prajwal/EmpManagement/employees/models.py
@@ -17,7 +17,7 @@ class Employee(models.Model):
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
 	address = models.CharField(max_length=200)
-	emp_id = models.IntegerField()
+	emp_id = models.BigAutoField(primary_key=True)
 	mobile = models.CharField(max_length=10)
 	dept =models.CharField(max_length=100, choices=DEPARTMENT_CHOICES)
 	salary = models.IntegerField()
