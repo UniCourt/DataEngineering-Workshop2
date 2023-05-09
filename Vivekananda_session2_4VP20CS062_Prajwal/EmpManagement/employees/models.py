0f6a1b03b53543d295c583ee746a20732773ed16
Prajwal <prajwal@Ubuntu1.myguest.virtualbox.org>
2023-05-09 07:37:37 +0530

    modifications

--- Vivekananda_session2_4VP20CS062_Prajwal/EmpManagement/employees/models.py
+++ Vivekananda_session2_4VP20CS062_Prajwal/EmpManagement/employees/models.py
@@ -14,13 +14,13 @@ DEPARTMENT_CHOICES = (
 
 
 class Employee(models.Model):
-	first_name = models.CharField(max_length=200, default = None)
-	last_name = models.CharField(max_length=200, default = None)
-	address = models.CharField(max_length=200,default = None)
-	emp_id = models.IntegerField(default = None)
-	mobile = models.CharField(max_length=10, default = None)
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	emp_id = models.IntegerField()
+	mobile = models.CharField(max_length=10)
 	dept =models.CharField(max_length=100, choices=DEPARTMENT_CHOICES)
-	salary = models.IntegerField(default = None)
+	salary = models.IntegerField()
 	
 	def __str__(self):
 		return self.first_name + " " + self.last_name
