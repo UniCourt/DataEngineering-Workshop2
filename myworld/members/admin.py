7a8eb6c59fa2caffb2ed7965114006f32b664da4
Rajashree <rajashreek378@gmail.com>
2023-05-20 20:08:39 +0530

    commiting hw2

--- myworld/members/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Students
-
-class DjStudentAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "address", "roll_number", "mobile", "branch")
-    list_filter = ("branch",)
-
-# Register your models here.
-admin.site.register(Students, DjStudentAdmin)
--- myworld/members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('rest/student/<int:rolno>', views.StudentView.as_view()),
-    path('rest/student/', views.StudentView.as_view()),
-    path('rest/student/<str:branch>', views.StudentView.as_view()),
-]
