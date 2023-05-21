8710c8ad83a10d004876dad3198ff3ec9001d4c2
raghugowd <raghu96867@gmail.com>
2023-05-21 12:26:42 +0530

    removed

--- employee/Employee/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employees
-
-class EmpAdmin(admin.ModelAdmin):
-    list_display = ('emp_id','name','address','salary','dept')
-    list_filter=('dept',)
-
-admin.site.register(Employees,EmpAdmin)
