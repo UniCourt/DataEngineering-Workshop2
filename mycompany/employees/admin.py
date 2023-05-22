23756cd486b5e443d3ee2ddb53a8b4b476598cbe
raghugowd <raghu96867@gmail.com>
2023-05-22 19:39:23 +0530

    removed

--- mycompany/employees/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Employees
-
-class DjEmployeeAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "emp_id", "address", "mobile", "dept","salary",)
-    list_filter = ("dept",)
-
-# Register your models here.
-admin.site.register(Employees, DjEmployeeAdmin)
--- mycompany/employees/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('employees/<int:emp_id>', views.EmployeeView.as_view()),
-    path('employees/', views.EmployeeView.as_view()),
-    path('employees/<str:dept>', views.EmployeeView.as_view()),
-]
